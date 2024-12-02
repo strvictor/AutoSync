3eab68e061208ce233dfe476e95113df0c898116
strvictor <vicctor1009@gmail.com>
2024-12-02 15:04:30 -0300

    feat(Adição do Docker e Postsgres): :sparkles: Adicionado Docker e PS
    adicionei o postsgresql ao inves do dbsqlite3 e orquestrei isso no docker-compose.yml

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM python:3.12.7-alpine3.20
+
+WORKDIR /core
+
+COPY requeriments.txt /core/
+
+RUN pip install --no-cache-dir -r requeriments.txt
+
+COPY . /core/
+
+EXPOSE 8000
+
+CMD [ "sh", "-c", "python manage.py makemigrations && python manage.py migrate && python manage.py runserver 0.0.0.0:8000" ]
--- core/settings.py
+++ core/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = 'django-insecure-ns%r&2=bwzzxnu383rkx2h%ck_(8xds#$p_f76ly1901zk7ur%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -69,10 +69,15 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST', 'localhost'),
+        'PORT': '5432',
     }
 }
 
