6f30815c6ebb22dd40e205d2b6a052f47903c269
strvictor <vicctor1009@gmail.com>
2024-12-22 17:51:50 -0300

    feat(Dashboard): :bug: Implementando mais um dashboard
    O dash é Serviços por mes

--- home/views.py
+++ home/views.py
@@ -2,12 +2,14 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from servicos.models import Servicos
 from babel.numbers import format_currency
+from django.db.models import Count
+
 
 from django.db.models import Sum, F, ExpressionWrapper, DecimalField
 from django.db.models.functions import TruncMonth
 from django.shortcuts import render
 from django.contrib import messages
-from servicos.models import Servicos
+from servicos.models import Servicos, ServicoCategoriaQuantidade
 
 
 def home(request):
@@ -50,7 +52,6 @@ def home(request):
 
     contexto = {}
 
-    # Gerar relatório de serviços finalizados agrupados por mês
     servicos_por_mes = (
         Servicos.objects.filter(status="Finalizado")
         .annotate(mes=TruncMonth('data_finalizacao'))  # Agrupar por mês
@@ -65,13 +66,13 @@ def home(request):
         .order_by('mes')
     )
 
-    print(servicos_por_mes)
-
     # Preparar dados para o gráfico
     orders_month_report = [
         {'mes': servico['mes'].strftime('%Y-%m'), 'total_valor': float(servico['total_valor'])}
         for servico in servicos_por_mes
     ]
+
+
     orders_month_report_labels = [item['mes'] for item in orders_month_report]
     orders_month_report_data = [item['total_valor'] for item in orders_month_report]
 
@@ -95,6 +96,38 @@ def home(request):
     contexto['valores_meses'] = orders_month_report_data
     contexto['nomes_meses'] = nomes_meses
     contexto['dados_cards'] = dados_cards
+
+
+
+
+
+    servicos_por_categoria_mes = (
+    ServicoCategoriaQuantidade.objects.filter(servico__status="Finalizado")
+    .annotate(mes=TruncMonth('servico__data_finalizacao'))  # Agrupar por mês
+    .values('mes', 'categoria__titulo')  # Selecionar mês e categoria
+    .annotate(total_servicos=Count('id'))  # Contar os serviços por categoria
+    .order_by('mes', 'categoria__titulo')  # Ordenar por mês e por categoria
+    )
+
+    # Mapear o resultado para três listas separadas
+    meses_abreviados = {
+    1: "jan", 2: "fev", 3: "mar", 4: "abr", 5: "mai", 6: "jun",
+    7: "jul", 8: "ago", 9: "set", 10: "out", 11: "nov", 12: "dez"
+    }
+
+    # Extrair os valores
+    meses = [meses_abreviados[servico['mes'].month] for servico in servicos_por_categoria_mes]
+    categorias = [servico['categoria__titulo'] for servico in servicos_por_categoria_mes]
+    quantidade_servicos = [servico['total_servicos'] for servico in servicos_por_categoria_mes]
+
+    meses_retorno = []
+    for mes in meses:
+        if mes not in meses_retorno:
+            meses_retorno.append(mes)
+
+    contexto['meses_retorno'] = meses_retorno
+    contexto['categorias'] = categorias
+    contexto['quantidade_servicos'] = quantidade_servicos
 
     print(contexto)
 
