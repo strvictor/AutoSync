3132b8e29a8fbf02b00b090efc459abbfedf1770
strvictor <vicctor1009@gmail.com>
2025-04-30 11:42:47 -0300

    feat: :sparkles: Filtrar estoque e reabastecimentos por empresa

--- estoque/views.py
+++ estoque/views.py
@@ -7,10 +7,10 @@ from django.db.models import F
 
 def estoque(request):
     if request.method == "GET":
-        estoque = Estoque.objects.all()
+        estoque = Estoque.objects.filter(empresa=request.empresa)
         qtd_itens = len(estoque)
-        qtd_minima = Estoque.objects.filter(quantidade_em_estoque__lt=F('quantidade_minima')).count()
-        ultima_atualizacao = Estoque.objects.order_by('-atualizado_em').first().atualizado_em.strftime("%d/%m/%Y %H:%M") if Estoque.objects.exists() else None
+        qtd_minima = Estoque.objects.filter(empresa=request.empresa, quantidade_em_estoque__lt=F('quantidade_minima')).count()
+        ultima_atualizacao = Estoque.objects.filter(empresa=request.empresa).order_by('-atualizado_em').first().atualizado_em.strftime("%d/%m/%Y %H:%M") if Estoque.objects.filter(empresa=request.empresa).exists() else None
         
         return render(request, "estoque.html", {"qtd_itens": qtd_itens, 'qtd_minima': qtd_minima, 'ultima_atualizacao': ultima_atualizacao})
 
@@ -20,10 +20,10 @@ def reabastecer_estoque(request):
         item_id = request.POST.get("item")
         quantidade = int(request.POST.get("quantidade"))
 
-        item = get_object_or_404(Estoque, id=item_id)
+        item = get_object_or_404(Estoque, id=item_id, empresa=request.empresa)
 
         # cria histórico e atualiza estoque
-        Reabastecimento.objects.create(item=item, quantidade_adicionada=quantidade)
+        Reabastecimento.objects.create(item=item, quantidade_adicionada=quantidade, empresa=request.empresa)
         item.quantidade_em_estoque += quantidade
         item.save()
 
@@ -36,11 +36,11 @@ def reabastecer_estoque(request):
     item_quantidade = None
 
     if item_id:
-        item = get_object_or_404(Estoque, id=item_id)
+        item = get_object_or_404(Estoque, id=item_id, empresa=request.empresa)
         item_nome = item.nome
         item_quantidade = item.quantidade_em_estoque
 
-    estoque = Estoque.objects.select_related('nome').all()
+    estoque = Estoque.objects.filter(empresa=request.empresa).select_related('nome').all()
 
     return render(request, "reabastecer.html", {
         "estoque": estoque,
@@ -53,7 +53,7 @@ def reabastecer_estoque(request):
 def quantidade_minima(request):
     if request.method == "GET":
 
-        estoque = Estoque.objects.filter(quantidade_em_estoque__lt=F('quantidade_minima'))
+        estoque = Estoque.objects.filter(empresa=request.empresa, quantidade_em_estoque__lt=F('quantidade_minima'))
         for c in estoque:
             print(f'{c.nome}, {c.quantidade_em_estoque}')
         return render(request, "quantidade_minima.html", {"estoque": estoque})
@@ -61,5 +61,5 @@ def quantidade_minima(request):
     
 def ultimos_reabastecimentos(request):
     if request.method == "GET":
-        reabastecimentos = Reabastecimento.objects.all()
+        reabastecimentos = Reabastecimento.objects.filter(empresa=request.empresa)
         return render(request, "ultimos_reabastecimentos.html", {"reabastecimentos": reabastecimentos})
